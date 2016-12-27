427ad71cf19c2fcceda788e4d96dc11dae8b04d8
29alpha <29a@jessewimberley.com>
2016-12-27 00:46:31 -0800

    Add files via upload

---
+++ MacChanger/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace MacChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbNICs = new System.Windows.Forms.ComboBox();
+            this.lblNICs = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.lblMAC = new System.Windows.Forms.Label();
+            this.txtMAC = new System.Windows.Forms.TextBox();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnRevert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbNICs
+            // 
+            this.cbNICs.AllowDrop = true;
+            this.cbNICs.FormattingEnabled = true;
+            this.cbNICs.Location = new System.Drawing.Point(12, 34);
+            this.cbNICs.Name = "cbNICs";
+            this.cbNICs.Size = new System.Drawing.Size(241, 21);
+            this.cbNICs.TabIndex = 0;
+            // 
+            // lblNICs
+            // 
+            this.lblNICs.AutoSize = true;
+            this.lblNICs.Location = new System.Drawing.Point(12, 18);
+            this.lblNICs.Name = "lblNICs";
+            this.lblNICs.Size = new System.Drawing.Size(100, 13);
+            this.lblNICs.TabIndex = 1;
+            this.lblNICs.Text = "Network Interfaces:";
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(13, 64);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(122, 13);
+            this.lblType.TabIndex = 3;
+            this.lblType.Text = "Network Interface Type:";
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(74, 87);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(61, 13);
+            this.lblIP.TabIndex = 4;
+            this.lblIP.Text = "IP Address:";
+            // 
+            // lblMAC
+            // 
+            this.lblMAC.AutoSize = true;
+            this.lblMAC.Location = new System.Drawing.Point(61, 113);
+            this.lblMAC.Name = "lblMAC";
+            this.lblMAC.Size = new System.Drawing.Size(74, 13);
+            this.lblMAC.TabIndex = 5;
+            this.lblMAC.Text = "MAC Address:";
+            // 
+            // txtMAC
+            // 
+            this.txtMAC.Location = new System.Drawing.Point(141, 110);
+            this.txtMAC.Name = "txtMAC";
+            this.txtMAC.Size = new System.Drawing.Size(111, 20);
+            this.txtMAC.TabIndex = 6;
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(124, 142);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(128, 34);
+            this.btnChange.TabIndex = 7;
+            this.btnChange.Text = "Change MAC";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // btnRevert
+            // 
+            this.btnRevert.Enabled = false;
+            this.btnRevert.Location = new System.Drawing.Point(16, 142);
+            this.btnRevert.Name = "btnRevert";
+            this.btnRevert.Size = new System.Drawing.Size(102, 34);
+            this.btnRevert.TabIndex = 8;
+            this.btnRevert.Text = "Revert";
+            this.btnRevert.UseVisualStyleBackColor = true;
+            this.btnRevert.Click += new System.EventHandler(this.btnRevert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 188);
+            this.Controls.Add(this.btnRevert);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.txtMAC);
+            this.Controls.Add(this.lblMAC);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.lblNICs);
+            this.Controls.Add(this.cbNICs);
+            this.Name = "Form1";
+            this.Text = "MAC Changer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbNICs;
+        private System.Windows.Forms.Label lblNICs;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Label lblMAC;
+        private System.Windows.Forms.TextBox txtMAC;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnRevert;
+    }
+}
+
---
+++ MacChanger/Form1.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace MacChanger
+{
+    public partial class Form1 : Form
+    {
+        // Getting the Global IP properties of the Local Machine
+        IPGlobalProperties boxProperties = IPGlobalProperties.GetIPGlobalProperties();
+        // Creating an array of all the NICs
+        NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+        // Create class-scoped variables to hold NIC ID (from NetworkInterface) and Name (Description from WMI Network Adapter)
+        string nicID, nicName;
+        // Declaring keys in the class scope
+        RegistryKey hklm, mackey;
+        public Form1()
+        {
+            InitializeComponent();
+            // Escalating privileges to write to HKLM.  Did not resposibly revert.  Bad code monkey.
+            new System.Security.Permissions.RegistryPermission(System.Security.Permissions.PermissionState.Unrestricted).Assert();
+            // Buiding RegistryKeys (class-scoped), in the same scope as the Assert.
+            hklm = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry32);
+            mackey = hklm.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}", true);
+            // Grabbing all the interfaces
+            GetNetworkInterfaces();
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // DataBinding to Form controls
+            populateControls();
+        }
+
+        protected void GetNetworkInterfaces()
+        {
+            // IList needed to bind to DataSource
+            IList<NetworkInterface> listNics = new List<NetworkInterface>();
+            foreach (NetworkInterface nic in nics)
+            {
+                listNics.Add(nic);
+            }
+            // Bind the NICs by Name to the Drop Down Box
+            // Returns the ID of the selected NIC
+            cbNICs.DataSource = listNics;
+            cbNICs.DisplayMember = "Description";
+            cbNICs.ValueMember = "ID";
+            // Event Handler to update Form controls when selection changes
+            cbNICs.SelectionChangeCommitted += new EventHandler(populateControls);
+        }
+        private void populateControls()
+        {
+            // Setting Revert button to disabled
+            btnRevert.Enabled = false;
+             NetworkInterface selected = nics[(cbNICs.SelectedIndex)];
+            // Setting global variables
+            nicID = selected.Id;
+            nicName = selected.Description;
+            // Updating controls
+            lblType.Text = "Network Interface Type: " + selected.NetworkInterfaceType;
+            lblIP.Text = "IP Address: " + selected.GetIPProperties().UnicastAddresses[0].Address.MapToIPv4().ToString();
+            // TODO: Why doesn't this value update after changing MAC?
+            txtMAC.Text = selected.GetPhysicalAddress().ToString();
+            // Enabling the Revert button if there is a MAC spoofing key in the Registry
+            foreach(string subKeyName in mackey.GetSubKeyNames()) {
+                RegistryKey regnic = mackey.OpenSubKey(subKeyName);
+                // Fitlering Software Loopback Interfaces, which return null instead of a NetworkInterface
+                if((regnic.GetValue("NetCfgInstanceID")) == null) {
+                    break;
+                    }
+                else {
+                    // Matching the ID Property of the Network Interface to the RegistryKey value 
+                    if(selected.Id == regnic.GetValue("NetCfgInstanceID").ToString()) {
+                        if(regnic.GetValue("NetworkAddress") == null) {
+                            break;
+                            }
+                        else {
+                            btnRevert.Enabled = true;
+                            break;
+                            }
+                        }
+                    }
+                }
+        }
+
+        // This overload is required to tie the method to an event handler.
+        // It just makes a call to the parameterless method.
+        private void populateControls(object sender, EventArgs e)
+        {
+            populateControls();
+        }
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            // Matching the Network Interface ID property to a RegistryKey value
+            foreach (string subKeyName in mackey.GetSubKeyNames())
+            {
+                RegistryKey regnic = hklm.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}\" + subKeyName, RegistryKeyPermissionCheck.ReadWriteSubTree);
+                if ((string)regnic.GetValue("NetCfgInstanceID") == nicID)
+                {
+                    // Adding the RegistryKey value to spoof MAC
+                    regnic.SetValue("NetworkAddress", txtMAC.Text);
+                    // Changes take affect after resetting the Adapter
+                    btnChange.Text = "One moment...";
+                    resetAdapter();
+                    btnChange.Text = "Change MAC";
+                    btnRevert.Enabled = true;
+                    RefreshInterfaces();
+                    break;
+                }
+
+            }
+        }
+
+        private void RefreshInterfaces() {
+
+            
+            }
+        private void btnRevert_Click(object sender, EventArgs e)
+        {
+            // Matching the Network Interface ID property to a RegistryKey value
+            foreach (string subKeyName in mackey.GetSubKeyNames())
+            {
+                RegistryKey regnic = hklm.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002BE10318}\" + subKeyName, RegistryKeyPermissionCheck.ReadWriteSubTree);
+                if ((string)regnic.GetValue("NetCfgInstanceID") == nicID)
+                {
+                    if (regnic.GetValue("NetworkAddress").ToString() == null)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        // Deleting the RegistryKey Value reverts to the hardware-defined MAC
+                        regnic.DeleteValue("NetworkAddress");
+                        // Changes take effect after the adapter is reset
+                        btnRevert.Text = "One moment....";
+                        resetAdapter();
+                        btnRevert.Text = "Revert";
+                        btnRevert.Enabled = false;
+                        break;
+                    }
+                }
+
+            }
+        }
+        // Disables and Enables the adapter so the MAC change takes effect
+        private void resetAdapter()
+        {
+            // Getting a collection (of one) Network Adapter from WMI
+            ManagementObject currentMObject = new ManagementObject();
+            string strWQuery = "SELECT * FROM Win32_NetworkAdapter WHERE Name = " + "'" + nicName + "'";
+            ObjectQuery oQuery = new System.Management.ObjectQuery(strWQuery);
+            ManagementObjectSearcher oSearcher = new ManagementObjectSearcher(oQuery);
+            ManagementObjectCollection coladapters = oSearcher.Get();
+            // Disable and Enable the selected adapter.
+            foreach (ManagementObject mo in coladapters)
+            {
+                mo.InvokeMethod("Disable", null);
+                mo.InvokeMethod("Enable", null);
+            }
+        }
+    }
+        }
+    
+
